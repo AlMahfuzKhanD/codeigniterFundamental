3fd0a8b4ffdb6bdbe04d42653b31e5eb6fcc8f3c
mahfuz380 <almahfuz380@gmail.com>
2020-06-13 10:05:53 +0600

    edit admin created

---
+++ application/views/pages/edit_admin.php
@@ -0,0 +1,31 @@
+<div class="box span12">
+					
+					<div class="box-content">
+						<form class="form-horizontal" action="" method="post">
+						  <fieldset>
+							
+							<div class="control-group">
+							  <label class="control-label" for="date01">Name</label>
+							  <div class="controls">
+								<input type="text" class="input-xlarge" >
+							  </div>
+							</div>
+
+							
+
+							<div class="control-group">
+							  <label class="control-label" for="fileInput">Admin Image</label>
+							  <div class="controls">
+								<input class="input-file uniform_on" id="fileInput" type="file">
+							  </div>
+							</div>          
+							
+							<div class="form-actions">
+							  <button type="submit" class="btn btn-primary">Edit Admin</button>
+							  <button type="reset" class="btn">Cancel</button>
+							</div>
+						  </fieldset>
+						</form>   
+
+					</div>
+				</div><!--/span-->
