7ed3c36bf7750eb9734a3eaa3c8affda51bf9da8
mahfuz380 <almahfuz380@gmail.com>
2020-06-13 10:04:46 +0600

    add student created

---
+++ application/views/pages/add_student.php
@@ -0,0 +1,43 @@
+<div class="box span12">
+					
+					<div class="box-content">
+						<form class="form-horizontal" action="" method="post">
+						  <fieldset>
+							
+							<div class="control-group">
+							  <label class="control-label" for="date01">Name</label>
+							  <div class="controls">
+								<input type="text" class="input-xlarge" >
+							  </div>
+							</div>
+
+							<div class="control-group">
+							  <label class="control-label" for="date01">Phone</label>
+							  <div class="controls">
+								<input type="text" class="input-xlarge" >
+							  </div>
+							</div>
+
+							<div class="control-group">
+							  <label class="control-label" for="date01">Roll</label>
+							  <div class="controls">
+								<input type="text" class="input-xlarge" >
+							  </div>
+							</div>
+
+							<!-- <div class="control-group">
+							  <label class="control-label" for="fileInput">File input</label>
+							  <div class="controls">
+								<input class="input-file uniform_on" id="fileInput" type="file">
+							  </div>
+							</div>    -->       
+							
+							<div class="form-actions">
+							  <button type="submit" class="btn btn-primary">Add Student</button>
+							  <button type="reset" class="btn">Cancel</button>
+							</div>
+						  </fieldset>
+						</form>   
+
+					</div>
+				</div><!--/span-->
