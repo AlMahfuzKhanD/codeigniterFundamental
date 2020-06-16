f69aa19c733124eef87ccd29bd0c5922fb45bd30
mahfuz380 <almahfuz380@gmail.com>
2020-06-16 08:09:09 +0600

    manage student created

---
+++ application/views/pages/manage_student.php
@@ -0,0 +1,23 @@
+
+
+<div class="container">
+            
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Student Name</th>
+        <th>Student Roll</th>
+        <th>Student Phone</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+
