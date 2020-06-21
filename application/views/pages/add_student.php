c31c48959ea698e023d55ca0cac3f401f625e07e
mahfuz380 <almahfuz380@gmail.com>
2020-06-21 08:51:37 +0600

    add student modified

--- application/views/pages/add_student.php
+++ application/views/pages/add_student.php
@@ -1,27 +1,41 @@
 <div class="box span12">
+
+					<?php
+
+					$message = $this->session->userdata('message');
+					if(!empty($message)){
+						echo "<span class='alert alert-danger'>$message</span>";
+						$this->session->unset_userdata('message');
+					}
+
+
+
+					?>
+
 					
 					<div class="box-content">
-						<form class="form-horizontal" action="" method="post">
+
+						<form class="form-horizontal" action="<?php base_url()?>save-student" method="post">
 						  <fieldset>
 							
 							<div class="control-group">
 							  <label class="control-label" for="date01">Name</label>
 							  <div class="controls">
-								<input type="text" class="input-xlarge" >
+								<input type="text"  name="student_name" class="input-xlarge" >
 							  </div>
 							</div>
 
 							<div class="control-group">
 							  <label class="control-label" for="date01">Phone</label>
 							  <div class="controls">
-								<input type="text" class="input-xlarge" >
+								<input type="text"  name="student_phone" class="input-xlarge" >
 							  </div>
 							</div>
 
 							<div class="control-group">
 							  <label class="control-label" for="date01">Roll</label>
 							  <div class="controls">
-								<input type="text" class="input-xlarge" >
+								<input type="text" name="student_roll" class="input-xlarge" >
 							  </div>
 							</div>
 
@@ -33,7 +47,7 @@
 							</div>    -->       
 							
 							<div class="form-actions">
-							  <button type="submit" class="btn btn-primary">Add Student</button>
+							  <button type="submit" name="add_student" class="btn btn-primary">Add Student</button>
 							  <button type="reset" class="btn">Cancel</button>
 							</div>
 						  </fieldset>
